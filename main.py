69bc2b92d43f23b8eec45f5741ebc8c64ffbea82
Chien-Yu Xiong <58307327+yugurt2005@users.noreply.github.com>
2024-06-09 22:16:43 -0700

    add host

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def home():
     print("Hello World")
 
-if __name__ == '__main__':
-    app.run(port=80)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
