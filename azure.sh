8ea567c15e71672d19d2b560253c7bbe939d2667
Chien-Yu Xiong <58307327+yugurt2005@users.noreply.github.com>
2024-06-12 13:56:10 -0700

    oauth

---
+++ azure.sh
@@ -0,0 +1,8 @@
+
+ssh -i "~/.ssh/azure.pem" azureuser@hacks.eastus.cloudapp.azure.com
+
+git pull origin main
+
+npm install
+
+
--- main.py
+++ main.py
@@ -1,13 +1,70 @@
-from flask import Flask
+import os
+import dotenv
+import flask
+import requests
 
-app = Flask(__name__)
+
+dotenv.load_dotenv()
+
+if os.environ.get("HACKATHON_MACHINE") == "VM":
+    site = "http://hacks.eastus.cloudapp.azure.com"
+    host = "0.0.0.0"
+    port = 80
+    debug = False
+else:
+    site = "http://localhost"
+    host = "127.0.0.1"
+    port = 8080
+    debug = True
+
+app = flask.Flask(__name__)
+
+app.config["OAUTH2"] = {
+    "google": {
+        "client_id": os.environ.get("GOOGLE_CLIENT_ID"),
+        "data": {
+            "url": "https://www.googleapis.com/oauth2/v3/userinfo",
+            "email": lambda json: json["email"],
+        },
+    }
+}
 
 
-@app.route("/")
-def home():
-    print("Hello World")
-    return "Hello World"
+def oauth2_verify() -> str:
+    access_token = flask.request.headers.get("Authorization")
+
+    if not access_token:
+        return None
+
+    provider = flask.request.args.get("provider")
+
+    if provider not in app.config["OAUTH2"]:
+        return None
+
+    metadata = app.config["OAUTH2"][provider]["data"]
+
+    response = requests.get(
+        metadata["url"],
+        headers={
+            "Authorization": access_token,
+            "Accept": "application/json",
+        },
+    )
+    if response.status_code != 200:
+        return None
+
+    return metadata["email"](response.json())
+
+
+@app.route("/restricted/endpoint")
+def restricted_endpoint():
+    email = oauth2_verify()
+
+    if email:
+        return flask.jsonify({"email": email})
+    else:
+        return flask.jsonify({"error": "Unauthorized"}), 401
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host=host, port=port, debug=debug)
